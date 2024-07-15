814a33920daa1fa08084fa429dd262bdcec8cc79
coolcreation <73644110+coolcreation@users.noreply.github.com>
2024-07-15 15:41:54 -0600

    fixing broken code releated to add function

--- math.test.js
+++ math.test.js
@@ -2,7 +2,7 @@ const { add, subtract, divide, multiply } = require("./math");
 
 describe("Math.js tests", () => {
   it("should test  add  ", () => {
-    expect(add(2, 2)).toBe(555555);
+    expect(add(2, 2)).toBe(4);
   });
 
   it("Should test subtract", () => {
